5354b4f7d3f2f4dbd35470cd58523d49439dc90f
krisharavsj <krisharsv@gmail.com>
2025-07-07 20:21:05 +0530

    datetime.py

---
+++ date and time.py
@@ -0,0 +1,7 @@
+import datetime
+from datetime import date, time, datetime
+date=date.today ()
+time=datetime.now()
+print("the time now is",date)
+print("the time now is",time)
+print(date.year,date.month,date.day)
